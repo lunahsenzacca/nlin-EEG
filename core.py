Correct normalization and scales + find_peaks

core.py
@@ -304,22 +304,27 @@ def list_toevoked(data_list: list, subID: str, exp_name: str, avg_trials: bool, 
         # Axis 1 = Electrodes
         # Axis 2 = Time points
 
-        # Apply zscore normalization
-        if z_score == True:
-
-            array = zscore(array)
-
         # Average across same condition trials
         if avg_trials == True:
 
             n_trials = array.shape[0]
             avg = array.mean(axis = 0)
 
-            ev = mne.EvokedArray(avg, info, nave = n_trials, comment = conditions[i])
+            # Apply Z-Score
+            if z_score == True:
+
+                avg = zscore(avg[np.newaxis])
+
+            ev = mne.EvokedArray(avg[0], info, nave = n_trials, comment = conditions[i])
             evokeds.append(ev)
 
         # Keep each individual trial
         else:
+
+            # Apply zscore normalization
+            if z_score == True:
+
+                array = zscore(array)
 
             for trl in array:
 
@@ -721,7 +726,7 @@ def corr_exp(log_csum: list, log_r: list, n_points: int):
 
     return  ce, n_log_r
 
-# Information Dimension for a singleembedded time series with 2NN-estimation [Krakovská-Chvosteková]
+# Information Dimension for a single embedded time series with 2NN-estimation [Krakovská-Chvosteková]
 def idim(emb_ts: np.ndarray, m_period: int):
 
     N = len(emb_ts)
@@ -888,8 +893,8 @@ def correlation_sum(evoked: mne.Evoked, ch_list: list|tuple,
 
                 for r in rvals:
 
-                    CD.append(corr_sum(emb_ts, r = r, m_norm = m_norm, m = np.sqrt(m)))
-
+                    CD.append(corr_sum(emb_ts, r = r, m_norm = m_norm, m = np.sqrt(len(emb_ts))))
+                    #print(len(emb_ts))
     else:
 
         TS = evoked.get_data(picks = ch_list)

corrsum.py
@@ -26,7 +26,7 @@ maind = get_maind()
 
 ### MULTIPROCESSING PARAMETERS ###
 
-workers = 16
+workers = 10
 chunksize = 1
 
 ### SCRIPT PARAMETERS ###
@@ -35,7 +35,7 @@ chunksize = 1
 exp_name = 'zbmasking'
 
 # Label for results folder
-lb = 'mCFPO'
+lb = 'VAN'
 
 # Get data averaged across trials
 avg_trials = True
@@ -79,18 +79,18 @@ ch_list = ['Fp1'],['Fp2'],['Fpz'],['Fp1', 'Fp2', 'Fpz'],['O2'],['PO4'],['PO8'],[
 ### PARAMETERS FOR CORRELATION SUM COMPUTATION ###
 
 # Embedding dimensions
-embeddings = [i for i in range(2,21)]
+embeddings = [i for i in range(3,7)]
 
 # Time delay
 tau = maind[exp_name]['tau']
 
 # Window of interest
-frc = [0, 1]
+frc = [.1, .5]
 
 # Distances for sampling the dependance
 #r = np.logspace(0, 4.38, num = 27, base = 10)
 #r = r/1e9
-r = np.logspace(-2, 1, num = 20, base = 10)
+r = np.logspace(-1.7, 0.7, num = 30, base = 10)
 
 # Apply embedding normalization when computing distances
 m_norm = True

plotting.py
@@ -38,7 +38,8 @@ clust_dict = {'G': ['Global'],
               'znoisefree': ['Lorenz'],
               'm_znoisefree': ['Lorenz (m norm)'],
               'gnoise': ['Gaussian Noise'],
-              'm_gnoise': ['Gaussian Noise (m norm)']}
+              'm_gnoise': ['Gaussian Noise (m norm)'],
+              'test':['Fp1','Fp2','Fpz','Frontal','O2','PO4','PO8','Parieto-Occipital','Fronto-Parieto-Occipital System'],}
 
 obs_dict = {'corrsum': '$C_{m}(r)$ ',
             'correxp': '$\\nu_{m}(r)$ ',
@@ -62,7 +63,7 @@ def show_figure(fig):
 
     return
 
-def plot_1dfunction(OBS: np.ndarray, E_OBS: np.ndarray, X: list, label: list, alpha_m: float, grid: list, figsize: list):
+def plot_1dfunction(OBS: np.ndarray, E_OBS: np.ndarray, X: list, label: list, label_idxs: list, alpha_m: float, grid: list, figsize: list):
 
     # Scalar value 4-dimensional array
 
@@ -75,7 +76,7 @@ def plot_1dfunction(OBS: np.ndarray, E_OBS: np.ndarray, X: list, label: list, al
 
     cmap = cm.viridis
 
-    norm = mcolors.Normalize(vmin = 0, vmax = len(label) - 1)
+    norm = mcolors.Normalize(vmin = 0, vmax = len(label_idxs) - 1)
 
     fig, axs = plt.subplots(grid[0], grid[1], figsize = figsize, sharex = True, sharey = True)
 
@@ -86,9 +87,9 @@ def plot_1dfunction(OBS: np.ndarray, E_OBS: np.ndarray, X: list, label: list, al
 
     for j, ax in enumerate(ax_iter):
 
-        for c in range(0,obs.shape[1]):
+        for i, c in enumerate(label_idxs):
 
-            color = cmap(norm(c))
+            color = cmap(norm(i))
 
             if j == 0:
                 ax.plot(X, obs[j,c,:], color = color, alpha = 1*alpha_m, label = label[c])
@@ -183,9 +184,14 @@ def plot_observable(info: dict, instructions: dict, show = True, save = False, v
     if instructions['avg'] != 'none':
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
+
+            norm = OBS.shape[avg[instructions['avg']]]
+
+            if type(norm) != int:
+                norm = norm[0]*norm[1]
             
             OBS = np.nanmean(OBS, axis = avg[instructions['avg']])
-            E_OBS = np.nanmean(E_OBS, axis = avg[instructions['avg']])
+            E_OBS = np.sqrt(np.nansum(E_OBS**2, axis = avg[instructions['avg']]))/norm
             
             OBS = np.expand_dims(OBS, axis = avg[instructions['avg']])
             E_OBS = np.expand_dims(E_OBS, axis = avg[instructions['avg']])
@@ -223,7 +229,7 @@ def plot_observable(info: dict, instructions: dict, show = True, save = False, v
 
         rearrange[3] = 1
 
-        legend_l = [cond_dict[i] for i in labels[1]]
+        legend_l = labels[1]
 
     elif instructions['legend'] == 'embeddings':
 
@@ -251,6 +257,14 @@ def plot_observable(info: dict, instructions: dict, show = True, save = False, v
         if i not in rearrange:
             rearrange[0] = i
 
+    if instructions['reduce_legend'] != None:
+
+        label_idxs = instructions['reduce_legend']
+
+    else:
+
+        label_idxs = [i for i in range(0,len(legend_l))]
+
     # Rearrange arrays
     obs = np.permute_dims(OBS, rearrange)
     e_obs = np.permute_dims(E_OBS, rearrange)
@@ -275,7 +289,7 @@ def plot_observable(info: dict, instructions: dict, show = True, save = False, v
     for i in range(0, len(obs)):
     
         # Initialize figures
-        fig, axis = plot_1dfunction(OBS = OBS[i], E_OBS = E_OBS[i], X = X, label = legend_l, alpha_m = instructions['alpha_m'], grid = instructions['grid'], figsize = instructions['figsize'])
+        fig, axis = plot_1dfunction(OBS = OBS[i], E_OBS = E_OBS[i], X = X, label = legend_l, label_idxs = label_idxs, alpha_m = instructions['alpha_m'], grid = instructions['grid'], figsize = instructions['figsize'])
 
         figs.append(fig)
         axes.append(axis)