f9571ffad0f2db35ea53c45a24c11d79294bf49d
lunisLT <elle.rutigliano@gmail.com>
2025-12-29 18:07:18 +0100

    Working VSCode enviroment and MNE Browser

--- devenv.nix
+++ devenv.nix
@@ -4,6 +4,10 @@
   packages = with pkgs; [
     gcc
     libz
+    python312Packages.qtpy
+    python312Packages.pyqt6
+    python312Packages.pyside6
+    python312Packages.pyqtgraph
   ];
   languages.python.enable = true;
   languages.python.version = "3.12.2";
