8bc54a0dd3e7970b784acc59612063e30d4551f9
lunisLT <elle.rutigliano@gmail.com>
2025-12-31 15:27:57 +0100

    Updated python version

--- devenv.nix
+++ devenv.nix
@@ -10,7 +10,7 @@
     python312Packages.pyqtgraph
   ];
   languages.python.enable = true;
-  languages.python.version = "3.12.2";
+  languages.python.version = "3.13";
   languages.python.venv.enable = true;
   languages.python.poetry.enable = true;
   languages.python.poetry.install.enable = true;
